4b5203f889489021a2e4687f5340e69a96aa3266
Hendricks27 <wenjinzhang95@gmail.com>
2021-07-30 13:23:56 -0500

    Glylookup image issue

--- src/Application/GlyLookup/htmls/renderresult.js
+++ src/Application/GlyLookup/htmls/renderresult.js
@@ -37,35 +37,5 @@ function renderResultMore(){
     result_container.innerHTML += "<br><img src='"+imgurl+"'>";
     result_container.innerHTML += "<br><a href='https://glytoucan.org/Structures/Glycans/"+retrieve_result.result[0]+"'>"+retrieve_result.result[0]+"</a>"
 
-    if (result.length == 0){
-        return
-    }
-
-    let sub_table = "<br><table style='width: 100%; border: black solid 3px; font-size: 15px;'>";
-    sub_table += "" +
-            "<tr style='height: 28px; background-color: grey'><td>Results</td></tr>";
-
-
-    for (let i in result){
-        let lightstr = 'light';
-        let r = result[i];
-
-        if (i % 2 == 1){
-            lightstr = "";
-        }
-
-        let strictstr = ""
-        if (r[1]){
-            strictstr = " (strict)"
-        }
-
-        sub_table += "" +
-            "<tr style='background-color: "+lightstr+"grey'><td><img src='https://glymage.glyomics.org/image/snfg/extended/"+r[0]+".png' style='max-width: 100%; max-height: 150px;'><br><a href='https://glytoucan.org/Structures/Glycans/"+r[0]+"'>"+ r[0] +"</a> "+strictstr+"</td></tr>";
-
-    }
-
-    sub_table += "</table>";
-    result_container_additional.innerHTML += sub_table + "<br>";
-
 
 }
