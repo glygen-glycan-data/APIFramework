fcbb9c4ca3761548bcba009e9598b26eef085de6
Nathan Edwards <edwardsnj@gmail.com>
2023-01-17 16:14:07 -0500

    needed more from PyGly, so new commit

--- src/AutoBuild/APIFramework/build.sh
+++ src/AutoBuild/APIFramework/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 tag=$1
 if [ -z "$1" ]; then
     echo "No tag is provided, NOT going to push to docker hub."
@@ -12,7 +14,9 @@ fi
 # COMMIT="5716c270f441bb08b77009b838e08f7e67ad74bf"
 # COMMIT="957e634483c5189b5f1907dee5c1df7dccfd8187"
 # COMMIT="3c5c9642b77bd0dba67bee707464f0c716ef716a"
-COMMIT="bc58d18ba8e80b569b33021480fb26d9a4b9327a"
+# COMMIT="bc58d18ba8e80b569b33021480fb26d9a4b9327a"
+COMMIT="193eac123a73881e5a6b7e05e6bda78f26070d89"
+
 wget https://github.com/glygen-glycan-data/PyGly/archive/${COMMIT}.zip -O PyGly.zip
 unzip PyGly.zip
 mv ./PyGly-$COMMIT/pygly ./pygly
@@ -26,7 +30,7 @@ if [ "$tag" != "TEST" ]; then
     docker build -t glyomics/apiframework:$tag -t glyomics/apiframework:latest ./
     docker push glyomics/apiframework:$tag
     docker push glyomics/apiframework:latest
-else:
+else
     docker build -t glyomics/apiframework:latest ./
 fi
 
