e0f6763fc3d86d8b29b3af315d736095252b7a12
Nicole Mathias <nm1058@georgetown.edu>
2025-05-14 13:55:42 -0400

    added latest commit

--- src/AutoBuild/Extractor/build.sh
+++ src/AutoBuild/Extractor/build.sh
@@ -13,7 +13,8 @@ fi
 # COMMIT="94a9f575a5fa3b6320e4e1759537d87f05a2bc89"
 # COMMIT="c936f09fc48ec44c0cf73c2b28f82f013edab65f"
 # COMMIT="0b5de59b244798d8eb86e0e15e173e9087c9cd0f"
-COMMIT="57cfc0e413ec1917a09616f41c664e9fbe62008b"
+# COMMIT="57cfc0e413ec1917a09616f41c664e9fbe62008b"
+COMMIT="9e3d58b2da819cf7d782fa6cb401150bbf3af1b5"
 
 wget https://github.com/glygen-glycan-data/GlycanImageExtract2/archive/${COMMIT}.zip -O ImgExtractor.zip
 unzip -o ImgExtractor.zip
